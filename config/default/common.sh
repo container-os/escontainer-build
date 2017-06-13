66a1ba0a54f13c11e9a9f629dfe539938422649a
Chung-Ju Wu <jason.wu@easystack.cn>
2017-06-13 13:03:52 +0800

    Our ESCore repo requires username and password authentication.
    Default repo server is "mirror.easystack.io".
    Set username and password if our ESCore repo requires authentication.
    Explicitly assign username and password in the http protocol to access ESCore repo locations.
    
    Signed-off-by: Chung-Ju Wu <jason.wu@easystack.cn>
    Acked-by: Liang Chen <liang.chen@easystack.cn>

--- config/default/common.sh
+++ config/default/common.sh
@@ -10,10 +10,12 @@ export LANG=en_US
 # In additon, several repo locations are also provided based on SERVER.
 # Those fields will be used in setup.sh and build_iso.sh for accessing
 # all the rpm packages.
-SERVER=localhost
-SERVER_OS_REPO=http://${SERVER}/ESCore/CentOS/7.3.1611/os/x86_64/
-SERVER_UPDATES_REPO=http://${SERVER}/ESCore/CentOS/7.3.1611/updates/x86_64/
-SERVER_EASYSTACK_REPO=http://${SERVER}/ESCore/CentOS/7.3.1611/easystack/x86_64/
+SERVER=mirror.easystack.io
+USERNAME=escore
+PASSWORD=escore
+SERVER_OS_REPO=http://${USERNAME}:${PASSWORD}@${SERVER}/ESCore/CentOS/7.3.1611/os/x86_64/
+SERVER_UPDATES_REPO=http://${USERNAME}:${PASSWORD}@${SERVER}/ESCore/CentOS/7.3.1611/updates/x86_64/
+SERVER_EASYSTACK_REPO=http://${USERNAME}:${PASSWORD}@${SERVER}/ESCore/CentOS/7.3.1611/easystack/x86_64/
 
 # Set the git repo location and a list of required packages.
 REPO_LOCATION=git@github.com:easystack
