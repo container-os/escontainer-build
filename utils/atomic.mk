5b39ad658c901b87df5d50baaf40abf004a44bb3
Shawn Wang <shawn.wang@easystack.cn>
2017-08-09 18:00:37 +0800

    store atomic common variables into utils/atomic.mk
    Signed-off-by: Shawn Wang <shawn.wang@easystack.cn>
    Acked-by: Chen Fan <fan.chen@easystack.cn>

--- utils/atomic.mk
+++ utils/atomic.mk
@@ -1,2 +1,36 @@
+# Common variable
+
+OSTREE_REPO ?= /srv/ostree
+OSTREE_REPO_NAME ?= es-atomic-host
+OSTREE_BUILD_SCRIPTS_DIR ?= /srv/escore-build/atomic
+OSTREE_REPO_REF ?= $(DEFAULT_OSTREE_REPO_REF)
+OSTREE_SERV_HOST ?= 192.168.122.1
+OSTREE_SERV_PORT ?= 11887
+
+FORCE_COMPOSE ?= no
+
+DATE = $(shell date +%y%m%d)
+ifneq ($(wildcard ${OSTREE_REPO}/disk_${DATE}-[0-9][0-9]),)
+LAST_BUILD_NUM = $(shell ls -d ${OSTREE_REPO}/disk_${DATE}-[0-9][0-9] | sort -rn |head -n 1 | cut -d'-' -f 2)
+else
+LAST_BUILD_NUM = 00
+endif
+NEXT_BUILD_NUM = $(shell printf %02d `expr ${LAST_BUILD_NUM} + 1`)
+
+
+# Status
+OSTREE_INSTALLED = $(shell ostree --help > /dev/null 2>&1  && echo "yes" || echo "no")
+ifeq (yes,$(OSTREE_INSTALLED))
+RPM_OSTREE_INSTALLED = $(shell rpm-ostree --help > /dev/null 2>&1  && echo "yes" || echo "no")
+RPM_OSTREE_TOOLBOX_INSTALLED = $(shell rpm-ostree-toolbox > /dev/null 2>&1  && echo "yes" || echo "no")
+
+OSTREE_REPO_CREATED = $(shell test -e ${OSTREE_REPO} && echo "yes" || echo "no")
+OSTREE_REPO_SERVICE_PORT_USED = $(shell utils/check_port.py --host ${OSTREE_SERV_HOST} --port ${OSTREE_SERV_PORT})
+OSTREE_REPO_SERVICE_STARTED = $(shell utils/check_port.py --host ${OSTREE_SERV_HOST} --port ${OSTREE_SERV_PORT})
+endif
+
+JSON_FILE = $(SEED)/es_${DATE}-${NEXT_BUILD_NUM}.json
+
+
 include utils/ostree.mk
 include utils/vm.mk
