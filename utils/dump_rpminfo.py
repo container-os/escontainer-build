52a4f93ae24e8063dc15b9d9d403ba80d87c62f2
Shawn Wang <shawn.wang@easystack.cn>
2017-08-17 11:37:23 +0800

    update koji repo url as http://mirror.es/ESCL/el7.alpha/os
    Signed-off-by: Shawn Wang <shawn.wang@easystack.cn>
    Acked-by: Chung-Ju Wu<jason.wu@easystack.cn>

--- utils/dump_rpminfo.py
+++ utils/dump_rpminfo.py
@@ -6,13 +6,13 @@ import tempfile
 
 import yum
 
-release = '7.3.1611'
 arch = 'x86_64'
-baseurl = "http://mirror.easystack.io/ESCL/"
+baseurl = "http://mirror.easystack.io/ESCL"
 
 
-def setup_repo(repoid, cache=None):
-    url = "{}/{}/{}/{}".format(baseurl, release, repoid, arch)
+def setup_repo(tag, cache=None):
+    repoid = tag.replace('/', '_')
+    url = "{}/{}/{}".format(baseurl, tag, arch)
 
     newrepo = yum.yumRepo.YumRepository(repoid)
     newrepo.name = repoid
@@ -35,8 +35,8 @@ def fetch_rpms(repo):
             'package': str(pkg),
             'name': pkg.name,
             'base_package_name': pkg.base_package_name,
-            'dist': repo.name,
-            'release': release,
+            'release': repo.name.split('_')[0],
+            'dist': repo.name.split('_')[-1],
             'srpm': pkg.sourcerpm,
             'version': pkg.version,
             'epoch': pkg.epoch
@@ -48,9 +48,10 @@ def fetch_rpms(repo):
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('tags', metavar='repo', nargs='*',
-                        default=['os', 'atomic'], 
-                        help='repo tag to dump rpms data')
+    parser.add_argument(
+        'tags', metavar='repo', nargs='*',
+        default=['7.3.1611/os', '7.3.1611/atomic'], 
+        help='repo tag to dump rpms data, example: el7.alpha/os')
     parser.add_argument('--cache',  help='cache dir')
     parser.add_argument('--output', '-o', default='escore/rpms.json')
     args = parser.parse_args()
